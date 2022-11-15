7f69a114eca95c8d036017f0dae1554e06ffcd98
340886936 <340886936@gapps.yrdsb.ca>
2022-11-15 15:08:18 -0500

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,66 @@
+class Student {
+
+    //Attributes
+    String firstName;
+    String lastName;
+    String email;
+    String responses;
+    String studentNumber;
+    int score;
+
+    //Constructor
+    public Student(){
+        this.score = 0;
+    }
+
+    //Setters
+    void setFirstName(String first){
+        this.firstName = first;
+    }
+
+    void setLastName(String last){
+        this.lastName = last;
+    }
+
+    void setEmail(String e){
+        this.email = e;
+    }
+
+    void setResponses(String res){
+        this.responses = res;
+    }
+
+    void setStudentNumber(String num){
+        this.studentNumber = num;
+    }
+
+    void setScore(int s){
+        this.score = s;
+    }
+
+    //Getters
+    String getFirstName(){
+        return firstName;
+    }
+
+    String getLastName(){
+        return lastName;
+    }
+
+    String getEmail(){
+        return email;
+    }
+
+    String getResponses(){
+        return responses;
+    }
+
+    String getStudentNumber(){
+        return studentNumber;
+    }
+
+    int getScore(){
+        return score;
+    }
+
+}
