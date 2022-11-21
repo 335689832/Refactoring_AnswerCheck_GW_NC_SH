c284734de62266913f379dc405a26570c5bcd53e
denshay <340741479@gapps.yrdsb.ca>
2022-11-21 09:25:53 -0500

    Update CompareData.java

--- CompareData.java
+++ CompareData.java
@@ -5,7 +5,7 @@ public class CompareData{
     
     public static void process(Student[] studentList, ArrayList<String> answerList){
         if (!(answerList == null)){
-            for(int i = 0; i < studentList.length; i++){
+            for(int i = 1; i < studentList.length; i++){
                 int score = 0;
                 for(int j = 0; j < studentList[i].getResponses().size(); j++){
                     ArrayList<String> studentResponse = studentList[i].getResponses();
@@ -14,7 +14,9 @@ public class CompareData{
                     }   
                 }
                 studentList[i].setScore(score);
-                System.out.println(score);
+                String name = studentList[i].getFirstName();
+                int amount = answerList.size();
+                System.out.println(name + " scored " + score + "/" + amount);
             }
         }
     }
