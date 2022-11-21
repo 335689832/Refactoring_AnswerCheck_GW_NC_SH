4a0b9e6bfb68e0613c23170974dd6c79155ad6a6
335689832 <gtswong2005@gmail.com>
2022-11-21 17:57:31 -0500

    Revert "Got rid of unnecessary imports"
    This reverts commit 703ee28ef207df0f882d61f06d1cfe620d008fc1.

--- FileWrite.java
+++ FileWrite.java
@@ -1,6 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
 import java.io.IOException;
 import java.io.FileWriter;
-
+import java.util.Scanner;
 
 public class FileWrite {
 
