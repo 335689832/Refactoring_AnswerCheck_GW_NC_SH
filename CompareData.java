622e4487d0fc30501127fdc022c80c576d416a6f
335689832 <gtswong2005@gmail.com>
2022-11-21 17:53:32 -0500

    Revert "Update CompareData.java"
    This reverts commit c284734de62266913f379dc405a26570c5bcd53e.

--- CompareData.java
+++ CompareData.java
@@ -4,7 +4,7 @@ public class CompareData{
     
     public static void process(Student[] studentList, ArrayList<String> answerList){
         if (!(answerList == null)){
-            for(int i = 1; i < studentList.length; i++){
+            for(int i = 0; i < studentList.length; i++){
                 int score = 0;
                 for(int j = 0; j < studentList[i].getResponses().size(); j++){
                     ArrayList<String> studentResponse = studentList[i].getResponses();
@@ -13,9 +13,7 @@ public class CompareData{
                     }   
                 }
                 studentList[i].setScore(score);
-                String name = studentList[i].getFirstName();
-                int amount = answerList.size();
-                System.out.println(name + " scored " + score + "/" + amount);
+                System.out.println(score);
             }
         }
     }
