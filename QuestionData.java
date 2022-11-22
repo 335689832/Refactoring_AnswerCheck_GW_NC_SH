f7076602878f0a9a4fc7376c4efa25037f746dda
335689832 <gtswong2005@gmail.com>
2022-11-21 19:19:12 -0500

    Questiones

--- QuestionData.java
+++ QuestionData.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class QuestionData implements Chain {
+public class QuestionData extends PopulateArray implements Chain {
     private Chain nextInChain;
     
     private ArrayList<String> questionData;
@@ -12,7 +12,16 @@ public class QuestionData implements Chain {
 
     public void process(Files request){
         if((request.getAData().equals("n/a"))){
-            
+            String[][] question2D = populateArray(request.getQData(), true, ",");
+            for(int i = 0; i < question2D.length; i++){
+                String temp = "";
+                for(int j = 0; j < question2D[i].length; j++){
+                    if(question2D[i][j].contains("=")){
+                        temp += question2D[i][j];
+                    }
+                }
+                questionData.add(temp);
+            }
         }
         else{
             System.out.println("File invalid");
